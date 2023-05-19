92e0cfbc9dc692de2e3a363b6156015ac01b585c
Gabriel Okamoto <gabrielokamoto@hotmail.com>
2023-05-19 19:16:02 -0300

    docs: document RawValue

--- src/RawValue.ts
+++ src/RawValue.ts
@@ -1,5 +1,8 @@
 import { JSPrimitive } from "./index";
 
+/**
+ * Used to wrap primitive values, so that they do not get transformed.
+ */
 export default class RawValue {
   constructor(public value: JSPrimitive) {}
 }
